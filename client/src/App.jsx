6092e84a90f73fcb733ba18c65bbe9d80e4ef078
Katleho14 <katlehomotloung394@gmail.com>
2025-03-10 08:15:54 +0200

    add flowbite-react dependency; import Header component in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
+import Header from './components/Header'
 const App = () => {
   return (
     <BrowserRouter>

b3852212cd18223795caceab20211d47f3e736f9
Katleho14 <katlehomotloung394@gmail.com>
2025-03-10 07:44:15 +0200

    initialize React app with Vite and Tailwind CSS; add basic routing and pages

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='border border-blue-500 p-4 rounded-md shadow-md'>
+      App
+      </div>
+  )
+}
+
+export default App

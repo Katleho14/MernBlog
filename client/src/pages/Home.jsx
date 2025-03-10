cfa8619e4d0e810c3cf1f1b3941f08ca48231607
Katleho14 <katlehomotloung394@gmail.com>
2025-03-10 08:10:40 +0200

    add routing and pages; implement Header component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,8 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
 import React from 'react'
 
 const Home = () => {
   return (
-   <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/projects" element={<Projects/>} />
-        <Route path="/signin" element={<Signin/>} />  
-        <Route path="/signup" element={<Signup/>} />
-      
-    </Routes>
-
-   </BrowserRouter>
+    <div>Home</div>
   )
 }
 

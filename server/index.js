cc3d938b9574e96bc4078830c2e123c128021dd8
Katleho14 <katlehomotloung394@gmail.com>
2025-03-11 11:28:32 +0200

    feat: integrate bcryptjs for password hashing in user signup process

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js'
 import authRoutes from './routes/auth.routes.js'
+
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI,)
@@ -10,7 +12,7 @@ mongoose.connect(process.env.MONGODB_URI,)
   .catch(err => console.error('Connection error', err));
 
 const app = express()
-
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');

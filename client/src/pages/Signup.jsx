d0b9777660f2c6c5ef8e893ce0b25b20285a424e
Katleho14 <katlehomotloung394@gmail.com>
2025-03-12 02:50:52 +0200

    feat: fix API call in signup form by adding body and logging response data

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -18,12 +18,16 @@ try {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      body: JSON.stringify(formData),
+
     },
-  })
+    body: JSON.stringify(formData),
+  });
+
+  const data = await res.json();
+  console.log(data);
   
 } catch (error) {
-  
+  console.log(error)
 }
 
 };

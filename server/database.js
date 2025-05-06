cfe32f039f3be23ab15a262e8664aa50ec8175a6
Katleho14 <katlehomotloung394@gmail.com>
2025-05-06 10:47:38 +0200

    update database configuration and add SQL tools connections

--- server/database.js
+++ server/database.js
@@ -3,10 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "mysql",
-  logging: false, // Set to true to see SQL queries
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 export default sequelize;
+
+
